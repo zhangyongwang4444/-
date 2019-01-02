6112cc1a8e28f884a3d55edb1a7bff6ff2117307
zhangyongwang <20225614@qq.com>
2019-01-02 15:26:39 +0800

    按照面向对象思想改写

--- tabs.js
+++ tabs.js
@@ -2,12 +2,19 @@
 // 写成 Tabs接口 
 function Tabs(selector) {
     this.elements = $(selector)
+    this.init()
+    this.bindEvents()
+}
+
+Tabs.prototype.init = function () {
     this.elements.each(function (index, element) {
         console.log(element)
         $(element).children('.tabs-bar').children('li').eq(0).addClass('active')
         $(element).children('.tabs-content').children('li').eq(0).addClass('active')
     })
+}
 
+Tabs.prototype.bindEvents = function () {
     this.elements.on('click', '.tabs-bar > li', function (e) {
         var $li = $(e.currentTarget)
         $li.addClass('active').siblings().removeClass('active')
@@ -16,5 +23,6 @@ function Tabs(selector) {
         $content.addClass('active').siblings().removeClass('active')
     })
 }
+
 
 var tabs = new Tabs('.tabs')
