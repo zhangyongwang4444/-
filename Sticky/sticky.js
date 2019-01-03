e8ffddb47d17d7704184202c066462840f6768ad
zhangyongwang <20225614@qq.com>
2019-01-03 09:03:12 +0800

    Sticky--button

--- Sticky/sticky.js
+++ Sticky/sticky.js
@@ -7,12 +7,27 @@
 
 // var sticky = new Sticky(selector, n)
 
+// setTimeout(function () {
+//     console.log($('button').offset())
+// }, 3000)
+
+
+var buttonOffset = $('button').offset()
 
 $(window).on('scroll', function () {
+
     var scrollY = window.scrollY
+
     if (scrollY > 0) {
         $('#topbar').addClass('sticky')
     } else {
         $('#topbar').removeClass('sticky')
     }
+
+    if (scrollY + 60 > buttonOffset.top) {
+        $('button').addClass('sticky')
+    } else {
+        $('button').removeClass('sticky')
+    }
 })
+
